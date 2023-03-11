b8f3454514f95e2580b081baf2061e2616289ff3
bgwyllie <bgwyllie@gmail.com>
2023-03-11 14:46:22 -0500

    Initial Commit

---
+++ mac_extensiLE/mac_extensiLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_extensiLEApp.swift
+//  mac_extensiLE
+//
+//  Created by Becca GW on 2023-03-11.
+//
+
+import SwiftUI
+
+@main
+struct mac_extensiLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
